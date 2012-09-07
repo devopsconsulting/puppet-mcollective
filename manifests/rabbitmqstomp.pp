64e8d8c72dbf955bbfe4ddf9f1bd1b733f84d849
Frank Spijkerman <frank.spijkerman@avira.com>
2012-09-07 10:43:28 +0000

    Made the installation of the rabbitmq server conditional on the stomp_host, if its localhost assume you want to install the rabbitmq server locally on the puppetmaster

--- manifests/rabbitmqstomp.pp
+++ manifests/rabbitmqstomp.pp
@@ -1,27 +1,30 @@
 class mcollective::rabbitmqstomp {
-    class{"rabbitmq::repo::apt":}
-    class { 'rabbitmq::server':
-      port              => '5673',
-      delete_guest_user => true,
-      config_stomp => true,
-    }
-    rabbitmq_user { 'mcollective':
-      admin    => true,
-      password => '_9_ehoE8ria1',
-      provider => 'rabbitmqctl',
-    }
-    rabbitmq_user_permissions {"mcollective@/":
-        configure_permission => '.*',
-          read_permission      => '.*',
-          write_permission     => '.*',
+    # TODO: create a special role for the rabbitmq
+    if $stomp_host == "localhost" {
+        class{"rabbitmq::repo::apt":}
+        class { 'rabbitmq::server':
+          port              => '5673',
+          delete_guest_user => true,
+          config_stomp => true,
+        }
+        rabbitmq_user { 'mcollective':
+          admin    => true,
+          password => '_9_ehoE8ria1',
           provider => 'rabbitmqctl',
-    }
-    rabbitmq_plugin {'rabbitmq_stomp':
-      ensure => present,
-      provider => 'rabbitmqplugins',
-    }
-    rabbitmq_plugin {"amqp_client":
-        ensure => present,
-        provider => 'rabbitmqplugins',
+        }
+        rabbitmq_user_permissions {"mcollective@/":
+            configure_permission => '.*',
+              read_permission      => '.*',
+              write_permission     => '.*',
+              provider => 'rabbitmqctl',
+        }
+        rabbitmq_plugin {'rabbitmq_stomp':
+          ensure => present,
+          provider => 'rabbitmqplugins',
+        }
+        rabbitmq_plugin {"amqp_client":
+            ensure => present,
+            provider => 'rabbitmqplugins',
+        }
     }
 }
