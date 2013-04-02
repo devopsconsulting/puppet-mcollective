28337b40173be99d45b2d4fcb8084417f28877a2
Lars van de Kerkhof <lars@permanentmarkers.nl>
2013-04-02 13:00:14 +0200

    ruby version does not entail a gempath with that version, so use vendor_ruby.

--- manifests/fixgempath.pp
+++ manifests/fixgempath.pp
@@ -3,11 +3,11 @@
 # if the os is using ruby 1.9 this will make apt fail.
 class mcollective::fixgempath {
     if versioncmp($::rubyversion, '1.9') >= 0 {
-        file {"/usr/lib/ruby/${::rubyversion}/mcollective":
+        file {"/usr/lib/ruby/vendor_ruby/mcollective":
             ensure => 'link',
             target => '/usr/lib/ruby/1.8/mcollective'
         } ->
-        file {"/usr/lib/ruby/${::rubyversion}/mcollective.rb":
+        file {"/usr/lib/ruby/vendor_ruby/mcollective.rb":
             ensure => 'link',
             target => '/usr/lib/ruby/1.8/mcollective.rb'
         }
